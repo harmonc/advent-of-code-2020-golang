86ef59ce1b67d36f4fc973e80b37ba2221b92f8b
Caleb Harmon <calebmharmon@gmail.com>
2020-12-03 10:49:23 -0800

    Day 3 Solutions

---
+++ day3/part2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+    //"strconv"
+)
+
+func main() {
+    data, err := ioutil.ReadFile("data2.txt")
+    if err != nil {
+        fmt.Println("File reading error", err)
+        return
+    }
+    arr := strings.Split(string(data),"\n")
+    result := countTrees(arr, 0, 0, 3, 1)
+    result *= countTrees(arr, 0, 0, 1, 1)
+    result *= countTrees(arr, 0, 0, 5, 1)
+    result *= countTrees(arr, 0, 0, 7, 1)
+    result *= countTrees(arr, 0, 0, 1, 2)
+    fmt.Println(result)
+}
+
+func countTrees(grid []string, xPos int, yPos int, dx int, dy int) int {
+    result := 0
+    x := xPos
+    y := yPos
+    for y<len(grid){
+        if(strings.Compare(string(grid[y][x]),"#")==0){
+            result++;
+        }
+        x += dx
+        x = x % len(grid[y])
+        y += dy
+    }
+    return result
+}
