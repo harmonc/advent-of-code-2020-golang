0d1aa86b8ff3cf0df1d9a14e3b12e965a00d000c
Caleb Harmon <calebmharmon@gmail.com>
2020-12-08 13:10:49 -0800

    Day 8 solution

---
+++ day8/part1.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+    "strconv"
+)
+
+type bag struct {
+    color string
+    count int
+}
+
+
+func main() {
+    data, err := ioutil.ReadFile("data2.txt")
+    if err != nil {
+        fmt.Println("File reading error", err)
+        return
+    }
+    
+    lines := strings.Split(string(data),"\n")
+    fmt.Println(lines)
+    ops := make([]string,0)
+    args := make([]int,0)
+    for _,value := range(lines) {
+        direction := strings.Split(value, " ")
+        num,_ := strconv.Atoi(direction[1])
+        ops = append(ops, direction[0])
+        args = append(args, num)
+    }
+    fmt.Println(ops,args)
+    visited := make([]bool,len(ops))
+    curr := 0
+    prevCurr := 0
+    acc := 0
+    terminate := false
+    for !terminate {
+        visited[curr] = true
+        prevCurr = curr
+        if strings.Compare(ops[curr],"acc") == 0{
+            acc = acc + args[curr]
+            curr = curr + 1
+        }else if strings.Compare(ops[curr],"jmp") == 0{
+            curr = curr + args[curr]
+        }else if strings.Compare(ops[curr],"nop") == 0{
+            curr = curr + 1
+        }
+        if curr >= len(visited){
+            terminate = true
+        } else if visited[curr]{
+            terminate = true
+        }
+    }
+    fmt.Println(prevCurr,curr,acc)
+}
+
