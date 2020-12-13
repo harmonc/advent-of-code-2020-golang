cddb5317cbf780d551cfe1a463b80f0f5ccf6c82
Caleb Harmon <calebmharmon@gmail.com>
2020-12-13 11:58:32 -0800

    Day 13 Part 1

---
+++ day13/part1.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+    "strconv"
+    "math"
+)
+
+type bag struct {
+    color string
+    count int
+}
+
+
+func main() {
+    data, err := ioutil.ReadFile("data2.txt")
+    if err != nil {
+        fmt.Println("File reading error", err)
+        return
+    }
+    
+    lines := strings.Split(string(data),"\n")
+    fmt.Println(Part1(lines))
+}
+
+func Part1(input []string) int {
+    t,_ := strconv.Atoi(input[0])
+    fmt.Println(t,t+1)
+    min := math.MaxInt32
+    result := 0
+    for _,value := range(strings.Split(input[1],",")){
+        fmt.Println(value)
+        if strings.Compare(value,"x") == 0{
+        }else{
+            num,_ := strconv.Atoi(value)
+            dist := num - t % num
+            if dist < min {
+                min = dist
+                result = num
+            }
+        }
+    }
+    return result * min
+}
